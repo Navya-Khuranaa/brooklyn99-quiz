21b51ae7454a97b3aa875f32d30b20849acb92bd
Navya-Khuranaa <navyakhurana0224@gmail.com>
2026-05-27 20:10:21 +0530

    fix typo in welcome message

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-print("Welcome to the. brooklyn 99 quiz")
+print("Welcome to the brooklyn 99 quiz")
 list_ques=["Who plays the lovable, goofball lead detective?","What is Charles Boyle's favorite holiday, which he refers to as Turkey Day?","What is Captain Raymond Holt's beloved dog named?","What 1988 action film inspired Detective Jake Peralta to become a cop?","Terry Jeffords has a famous addiction to which fruity, frozen treat?","What office supply item does Amy Santiago obsess over and collect?","Who is Jake Peralta's arch-nemesis, a famous and charming car thief?","What is the unusual name of Jake and Amy’s baby boy?"]
 list_ans=["jake peralta","thanksgiving","cheddar","die hard","yogurt","binders","doug judy","mac"]
 i=0
